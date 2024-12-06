992420b95c80e79d9bd9719f245311dd1eb4fd93
huongduyen3012 <duyen.vanthi.cit20@eiu.edu.vn>
2024-12-06 09:21:46 +0700

    feat(enhance-ui): adjust css for form

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,12 @@
-import './App.css'
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
     <>
-      <h1>
-        Hello World
-      </h1>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  server: {
+    port: 3000,
+    // proxy: {
+    //   "/warehouse": {
+    //     target: "http://localhost:4000/", // URL for `web-warehouse`
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/dashboard/, ""),
+    //   },
+    // },
+  },
+});
